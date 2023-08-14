23ffd34f7d587de9a82fda2484929c0da977a162
Stharley <sth4rley@gmail.com>
2023-08-13 23:51:39 -0300

    update template

--- template.cpp
+++ template.cpp
@@ -4,12 +4,28 @@ using namespace std;
 
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
 #define endl '\n'
+#define db(x) cout << #x << ": " << x << endl
+#define log(x) cout << x << endl
+
+#define printv(vector)                              \
+    {                                               \
+        cout << #vector << " = ";                   \
+        for (auto& it : vector) cout << it << " ";  \
+        cout << endl;                               \
+    }
+
+#define print_map(map)                              \
+    for (auto e : map)                              \
+        cout << e.first << " " << e.second << endl; \
+
 
 typedef long long ll;
 
-const int INF = 0x3f3f3f3f;
+const int INF = 0x3f3f3f3f; 
 const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 
 int main() { _
+
+
 	exit(0);
 }
